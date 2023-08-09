84a03239565ca19c4445975604fe0c7e1381c6b4
Tyrell <tysteg14@gmail.com>
2023-08-09 18:14:12 -0500

    learned arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+divide(2000, 100)
+
+const square = parameter1 => parameter1 * parameter1;
+
+const add = (parameter3, parameter4) => {
+    return parameter3 + parameter4;
+}
